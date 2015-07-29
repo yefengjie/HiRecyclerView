583ad606b3dd5068f88cc3c487a7cc262b0662c8
feng ye <yefeng.freedom@gmail.com>
2015-07-29 17:59:45 +0800

    delete display info

--- app/src/main/java/com/freedom/yefeng/yfrecyclerview/example/base/AppApplication.java
+++ app/src/main/java/com/freedom/yefeng/yfrecyclerview/example/base/AppApplication.java
@@ -2,18 +2,15 @@ package com.freedom.yefeng.yfrecyclerview.example.base;
 
 import android.app.Application;
 
-import com.freedom.yefeng.yfrecyclerview.libs.DisplayInfo;
-
 
 /**
  * Created by yee on 11/1/13.
- * <p>
+ * <p/>
  */
 public class AppApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
-        DisplayInfo.init(getApplicationContext());
     }
 }
