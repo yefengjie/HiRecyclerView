9036567b58663ae756c3823dd2283514d7c6e8b1
feng ye <yefeng.freedom@gmail.com>
2015-07-30 11:55:04 +0800

    edit maven upload

---
+++ yfrecyclerview/src/main/java/com/geek/logistics/yfrecyclerview/SimpleViewHolder.java
@@ -0,0 +1,14 @@
+package com.geek.logistics.yfrecyclerview;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by yefeng on 7/24/15.
+ * github:yefengfreedom
+ */
+public class SimpleViewHolder extends RecyclerView.ViewHolder {
+    public SimpleViewHolder(View itemView) {
+        super(itemView);
+    }
+}
